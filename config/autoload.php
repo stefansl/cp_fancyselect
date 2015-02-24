c732b797dd498f517f4e4357e1e79496a9e2b504
stefansl <ssl@clickpress.de>
2015-02-24 18:06:03 +0100

    Fancy Select
    init

---
+++ config/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'j_fancyselect'   => 'system/modules/cp_fancyselect/templates/jquery',
+	'moo_fancyselect' => 'system/modules/cp_fancyselect/templates/mootools',
+));
